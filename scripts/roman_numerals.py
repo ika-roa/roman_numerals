79f833f902e27753b43895bc5a0c94c4341e90e0
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-07 16:42:18 +0100

    feat: Add support for input number 4

--- scripts/roman_numerals.py
+++ scripts/roman_numerals.py
@@ -1,5 +1,8 @@
 def to_roman(number):
     result = ""
-    for i in range(number):
-        result += "I"
+    if number == 4:
+        result += "IV"
+    else:
+        for i in range(number):
+            result += "I"
     return result
