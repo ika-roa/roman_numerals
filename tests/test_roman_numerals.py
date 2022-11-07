feddebd21fb281ddff5aa53ff9ce05721ea7f74e
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-07 16:16:14 +0100

    refactor tests to reflect new function name

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -1,22 +1,22 @@
 import unittest
 
-from scripts.roman_numerals import calculate_roman_numeral
+from scripts.roman_numerals import to_roman
 
 
-class TestCalculateRomanNumeral(unittest.TestCase):
+class TestToRoman(unittest.TestCase):
 
-    def test_calculate_roman_num_for_1(self):
+    def test_to_roman_for_1(self):
         result = "I"
-        assert calculate_roman_numeral(1) == result
+        assert to_roman(1) == result
 
-    def test_calculate_roman_num_for_2(self):
+    def test_to_roman_for_2(self):
         result = "II"
-        assert calculate_roman_numeral(2) == result
+        assert to_roman(2) == result
 
-    def test_calculate_roman_num_for_3(self):
+    def test_to_roman_for_3(self):
         result = "III"
-        assert calculate_roman_numeral(3) == result
+        assert to_roman(3) == result
 
-    def test_calculate_roman_num_for_4(self):
+    def test_to_roman_for_4(self):
         result = "IV"
-        assert calculate_roman_numeral(4) == result
+        assert to_roman(4) == result
