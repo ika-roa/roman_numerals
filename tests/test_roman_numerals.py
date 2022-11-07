0b0a0c64360413aa00fbbc8a3899f7c2685ee9b1
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-07 16:07:51 +0100

    test: Add test for input=1

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -1,1 +1,10 @@
 import unittest
+
+from scripts.roman_numerals import calculate_roman_numeral
+
+
+class TestCalculateRomanNumeral(unittest.TestCase):
+
+    def test_calculate_roman_num_for_1(self):
+        result = "I"
+        assert calculate_roman_numeral(1) == result
