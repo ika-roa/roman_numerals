50fbccb1ad6826c5f092bc0272ab23b0d75a8509
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-08 14:29:53 +0100

    feat: Add support for number 9

--- scripts/roman_numerals.py
+++ scripts/roman_numerals.py
@@ -2,9 +2,14 @@ def to_roman(number):
     result = ""
     remaining = number
 
+    if remaining == 9:
+        result += "IX"
+        remaining -= 9
+
     if remaining >= 5:
         result += "V"
         remaining -= 5
+
     if remaining == 4:
         result += "IV"
     else:
