b9220f8daab7ae066dc36aadbf2c55a5627c6802
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-08 14:58:52 +0100

    refactor: Generalize logic even further to include I's

--- scripts/roman_numerals.py
+++ scripts/roman_numerals.py
@@ -8,24 +8,9 @@ def to_roman(number):
     remaining = number
 
     for arabic_digit, roman_digit in ROMAN_DIGITS:
-        if remaining >= arabic_digit:
+        while remaining >= arabic_digit:
             result = append_digit(result, roman_digit)
             remaining = reduce_number_by_found_digit(remaining, arabic_digit)
-
-    # if remaining >= 9:
-    #     result = append_digit(result, "IX")
-    #     remaining = reduce_number_by_found_digit(remaining, 9)
-    #
-    # if remaining >= 5:
-    #     result = append_digit(result, "V")
-    #     remaining = reduce_number_by_found_digit(remaining, 5)
-    #
-    # if remaining >= 4:
-    #     result = append_digit(result, "IV")
-    #     remaining = reduce_number_by_found_digit(remaining, 4)
-
-    for i in range(remaining):
-        result = append_digit(result, "I")
 
     return result
 
