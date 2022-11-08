e59be07835565fdd8531c7d0872107ed3f830040
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-08 14:57:01 +0100

    refactor: Generalize repeated if clauses into for loop

--- scripts/roman_numerals.py
+++ scripts/roman_numerals.py
@@ -1,18 +1,28 @@
+ROMAN_DIGITS = ((9, "IX"),
+                (5, "V"),
+                (4, "IV"),
+                (1, "I"))
+
 def to_roman(number):
     result = ""
     remaining = number
 
-    if remaining >= 9:
-        result = append_digit(result, "IX")
-        remaining = reduce_number_by_found_digit(remaining, 9)
+    for arabic_digit, roman_digit in ROMAN_DIGITS:
+        if remaining >= arabic_digit:
+            result = append_digit(result, roman_digit)
+            remaining = reduce_number_by_found_digit(remaining, arabic_digit)
 
-    if remaining >= 5:
-        result = append_digit(result, "V")
-        remaining = reduce_number_by_found_digit(remaining, 5)
-
-    if remaining >= 4:
-        result = append_digit(result, "IV")
-        remaining = reduce_number_by_found_digit(remaining, 4)
+    # if remaining >= 9:
+    #     result = append_digit(result, "IX")
+    #     remaining = reduce_number_by_found_digit(remaining, 9)
+    #
+    # if remaining >= 5:
+    #     result = append_digit(result, "V")
+    #     remaining = reduce_number_by_found_digit(remaining, 5)
+    #
+    # if remaining >= 4:
+    #     result = append_digit(result, "IV")
+    #     remaining = reduce_number_by_found_digit(remaining, 4)
 
     for i in range(remaining):
         result = append_digit(result, "I")
