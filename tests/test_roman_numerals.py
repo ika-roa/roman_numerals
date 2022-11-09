3d4b7be0b7d99247ad5ca012c6696c4537d23044
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-09 11:34:06 +0100

    test: Refactor tests to remove duplication

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -1,46 +1,65 @@
-import unittest
-
+import pytest
 from scripts.roman_numerals import to_roman
 
+KNOWN_VALUES = ((1, 'I'),
+                (2, 'II'),
+                (3, 'III'),
+                (4, 'IV'),
+                (5, 'V'),
+                (6, 'VI'),
+                (7, 'VII'),
+                (8, 'VIII'),
+                (9, 'IX'),
+                (10, 'X'),
+                (50, 'L'),
+                (100, 'C'),
+                (500, 'D'),
+                (1000, 'M'),
+                (31, 'XXXI'),
+                (148, 'CXLVIII'),
+                (294, 'CCXCIV'),
+                (312, 'CCCXII'),
+                (421, 'CDXXI'),
+                (528, 'DXXVIII'),
+                (621, 'DCXXI'),
+                (782, 'DCCLXXXII'),
+                (870, 'DCCCLXX'),
+                (941, 'CMXLI'),
+                (1043, 'MXLIII'),
+                (1110, 'MCX'),
+                (1226, 'MCCXXVI'),
+                (1301, 'MCCCI'),
+                (1485, 'MCDLXXXV'),
+                (1509, 'MDIX'),
+                (1607, 'MDCVII'),
+                (1754, 'MDCCLIV'),
+                (1832, 'MDCCCXXXII'),
+                (1993, 'MCMXCIII'),
+                (2074, 'MMLXXIV'),
+                (2152, 'MMCLII'),
+                (2212, 'MMCCXII'),
+                (2343, 'MMCCCXLIII'),
+                (2499, 'MMCDXCIX'),
+                (2574, 'MMDLXXIV'),
+                (2646, 'MMDCXLVI'),
+                (2723, 'MMDCCXXIII'),
+                (2892, 'MMDCCCXCII'),
+                (2975, 'MMCMLXXV'),
+                (3051, 'MMMLI'),
+                (3185, 'MMMCLXXXV'),
+                (3250, 'MMMCCL'),
+                (3313, 'MMMCCCXIII'),
+                (3408, 'MMMCDVIII'),
+                (3501, 'MMMDI'),
+                (3610, 'MMMDCX'),
+                (3743, 'MMMDCCXLIII'),
+                (3844, 'MMMDCCCXLIV'),
+                (3888, 'MMMDCCCLXXXVIII'),
+                (3940, 'MMMCMXL'),
+                (3999, 'MMMCMXCIX'),
+                )
 
-class TestToRoman(unittest.TestCase):
 
-    def test_to_roman_for_1(self):
-        result = "I"
-        assert to_roman(1) == result
-
-    def test_to_roman_for_2(self):
-        result = "II"
-        assert to_roman(2) == result
-
-    def test_to_roman_for_3(self):
-        result = "III"
-        assert to_roman(3) == result
-
-    def test_to_roman_for_4(self):
-        result = "IV"
-        assert to_roman(4) == result
-
-    def test_to_roman_for_5(self):
-        result = "V"
-        assert to_roman(5) == result
-
-    def test_to_roman_for_6(self):
-        result = "VI"
-        assert to_roman(6) == result
-
-    def test_to_roman_for_7(self):
-        result = "VII"
-        assert to_roman(7) == result
-
-    def test_to_roman_for_8(self):
-        result = "VIII"
-        assert to_roman(8) == result
-
-    def test_to_roman_for_9(self):
-        result = "IX"
-        assert to_roman(9) == result
-
-    def test_to_roman_for_10(self):
-        result = "X"
-        assert to_roman(10) == result
+@pytest.mark.parametrize("test_input, expected", KNOWN_VALUES)
+def test_to_roman(test_input, expected):
+    assert to_roman(test_input) == expected
