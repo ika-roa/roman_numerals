6de0bd43d515a8daa3e4ec0fb58090e7e43e3310
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-10 09:32:06 +0100

    refactor: Move all functions to class RomanNumeral

--- scripts/roman_numerals.py
+++ scripts/roman_numerals.py
@@ -13,41 +13,39 @@ ROMAN_DIGITS = ((1000, 'M'),
                 (1, "I"))
 
 
-def to_roman(number):
-    if input_is_valid(number):
-        result = ""
-        remaining = number
+class RomanNumeral:
+    def to_roman(self, number):
+        if self.input_is_valid(number):
+            result = ""
+            remaining = number
 
-        for arabic_digit, roman_digit in ROMAN_DIGITS:
-            while remaining >= arabic_digit:
-                result = append_digit(result, roman_digit)
-                remaining = reduce_number_by_found_digit(remaining, arabic_digit)
+            for arabic_digit, roman_digit in ROMAN_DIGITS:
+                while remaining >= arabic_digit:
+                    result = self.append_digit(result, roman_digit)
+                    remaining = self.reduce_number_by_found_digit(remaining, arabic_digit)
 
-    return result
+        return result
 
+    def input_is_valid(self, number):
+        if not isinstance(number, int):
+            raise TypeError("Only integer numbers can be converted to Roman numerals")
+        elif number < 0:
+            raise ValueError("Romans did not know negative numbers.")
+        elif number == 0:
+            raise ValueError("Romans did not know the number 0.")
+        elif number > 3999:
+            raise ValueError("This number is too big to convert.")
+        else:
+            return True
 
-def input_is_valid(number):
-    if not isinstance(number, int):
-        raise TypeError("Only integer numbers can be converted to Roman numerals")
-    elif number < 0:
-        raise ValueError("Romans did not know negative numbers.")
-    elif number == 0:
-        raise ValueError("Romans did not know the number 0.")
-    elif number > 3999:
-        raise ValueError("This number is too big to convert.")
-    else:
-        return True
+    def append_digit(self, result, roman_digit):
+        result += roman_digit
+        return result
 
-
-def append_digit(result, roman_digit):
-    result += roman_digit
-    return result
-
-
-def reduce_number_by_found_digit(remaining, arabic_digit):
-    remaining -= arabic_digit
-    return remaining
+    def reduce_number_by_found_digit(self, remaining, arabic_digit):
+        remaining -= arabic_digit
+        return remaining
 
 
 if __name__ == "__main__":
-    print(to_roman(2))
+    print(RomanNumeral().to_roman(number=2))
